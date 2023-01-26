7bec12c3d2b87192a2a3648a351e78709a93e5a7
hermannm <hermann.morkrid@gmail.com>
2023-01-26 19:05:46 +0100

    implement message receiving for ApiClient

--- Assets/Scripts/Api/ApiClient.cs
+++ Assets/Scripts/Api/ApiClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 using Immerse.BfhClient.Api.GameTypes;
 using Immerse.BfhClient.Api.Messages;
 using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace Immerse.BfhClient.Api
 {
@@ -29,15 +31,15 @@ namespace Immerse.BfhClient.Api
 
         private readonly BlockingCollection<ArraySegment<byte>> _sendQueue;
 
-        private ConcurrentQueue<ErrorMsg> _errorMessages;
-        private ConcurrentQueue<PlayerStatusMsg> _playerStatusMessages;
-        private ConcurrentQueue<LobbyJoinedMsg> _lobbyJoinedMessages;
-        private ConcurrentQueue<SupportRequestMsg> _supportRequestMessages;
-        private ConcurrentQueue<OrderRequestMsg> _orderRequestMessages;
-        private ConcurrentQueue<OrdersReceivedMsg> _ordersReceivedMessages;
-        private ConcurrentQueue<OrdersConfirmationMsg> _ordersConfirmationMessages;
-        private ConcurrentQueue<BattleResultsMsg> _battleResultsMessages;
-        private ConcurrentQueue<WinnerMsg> _winnerMessages;
+        private readonly ConcurrentQueue<ErrorMsg> _errorMessages;
+        private readonly ConcurrentQueue<PlayerStatusMsg> _playerStatusMessages;
+        private readonly ConcurrentQueue<LobbyJoinedMsg> _lobbyJoinedMessages;
+        private readonly ConcurrentQueue<SupportRequestMsg> _supportRequestMessages;
+        private readonly ConcurrentQueue<OrderRequestMsg> _orderRequestMessages;
+        private readonly ConcurrentQueue<OrdersReceivedMsg> _ordersReceivedMessages;
+        private readonly ConcurrentQueue<OrdersConfirmationMsg> _ordersConfirmationMessages;
+        private readonly ConcurrentQueue<BattleResultsMsg> _battleResultsMessages;
+        private readonly ConcurrentQueue<WinnerMsg> _winnerMessages;
 
         public ApiClient(string serverUri)
         {
@@ -57,7 +59,8 @@ namespace Immerse.BfhClient.Api
             _battleResultsMessages = new ConcurrentQueue<BattleResultsMsg>();
             _winnerMessages = new ConcurrentQueue<WinnerMsg>();
 
-            _sendThread = new Thread(SendFromQueue);
+            _sendThread = new Thread(SendMessagesFromQueue);
+            _receiveThread = new Thread(ReceiveMessagesIntoQueues);
         }
 
         public async Task Connect()
@@ -65,25 +68,22 @@ namespace Immerse.BfhClient.Api
             await _connection.ConnectAsync(_serverUri, CancellationToken.None);
         }
 
-        private async void SendFromQueue()
+        private async void SendMessagesFromQueue()
         {
-            while (true)
+            while (!_sendQueue.IsCompleted)
             {
-                while (!_sendQueue.IsCompleted)
-                {
-                    var message = _sendQueue.Take();
+                var message = _sendQueue.Take();
 
-                    await _connection.SendAsync(
-                        message,
-                        WebSocketMessageType.Text,
-                        true,
-                        CancellationToken.None
-                    );
-                }
+                await _connection.SendAsync(
+                    message,
+                    WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None
+                );
             }
         }
 
-        private void Send(string messageType, object message)
+        private void SendMessage(string messageType, object message)
         {
             var jsonMessage = new JObject(new JProperty(messageType, message)).ToString();
             var byteMessage = (ArraySegment<byte>) _encoding.GetBytes(jsonMessage);
@@ -92,42 +92,157 @@ namespace Immerse.BfhClient.Api
 
         public void SendSelectGameIDMessage(string gameID)
         {
-            Send(MessageType.SelectGameID, new SelectGameIDMsg(gameID));
+            SendMessage(MessageType.SelectGameID, new SelectGameIDMsg(gameID));
         }
 
         public void SendReadyMessage(bool ready)
         {
-            Send(MessageType.Ready, new ReadyMsg(ready));
+            SendMessage(MessageType.Ready, new ReadyMsg(ready));
         }
 
         public void SendStartGameMessage()
         {
-            Send(MessageType.StartGame, new StartGameMsg());
+            SendMessage(MessageType.StartGame, new StartGameMsg());
         }
 
         public void SendSubmitOrdersMessage(List<Order> orders)
         {
-            Send(MessageType.SubmitOrders, new SubmitOrdersMsg(orders));
+            SendMessage(MessageType.SubmitOrders, new SubmitOrdersMsg(orders));
         }
 
         public void SendGiveSupportMessage(string supportingArea, string supportedPlayer)
         {
-            Send(MessageType.GiveSupport, new GiveSupportMsg(supportingArea, supportedPlayer));
+            SendMessage(MessageType.GiveSupport, new GiveSupportMsg(supportingArea, supportedPlayer));
         }
 
         public void SendWinterVoteMessage(string player)
         {
-            Send(MessageType.WinterVote, new WinterVoteMsg(player));
+            SendMessage(MessageType.WinterVote, new WinterVoteMsg(player));
         }
 
         public void SendSwordMessage(string area, int battleIndex)
         {
-            Send(MessageType.Sword, new SwordMsg(area, battleIndex));
+            SendMessage(MessageType.Sword, new SwordMsg(area, battleIndex));
         }
 
         public void SendRavenMessage(string player)
         {
-            Send(MessageType.Raven, new RavenMsg(player));
+            SendMessage(MessageType.Raven, new RavenMsg(player));
+        }
+
+        private async void ReceiveMessagesIntoQueues()
+        {
+            while (_connection.State == WebSocketState.Open)
+            {
+                var memoryStream = new MemoryStream();
+                var isTextMessage = true;
+
+                while (true)
+                {
+                    var buffer = new ArraySegment<byte>(new byte[4 * 1024]);
+
+                    var chunkResult = await _connection.ReceiveAsync(buffer, CancellationToken.None);
+                    if (chunkResult.MessageType == WebSocketMessageType.Text)
+                    {
+                        isTextMessage = false;
+                        break;
+                    }
+
+                    memoryStream.Write(buffer.Array!, buffer.Offset, chunkResult.Count);
+
+                    if (chunkResult.EndOfMessage)
+                    {
+                        break;
+                    }
+                }
+
+                if (!isTextMessage)
+                {
+                    break;
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                using var reader = new StreamReader(memoryStream, Encoding.UTF8);
+                var messageString = reader.ReadToEnd();
+
+                try
+                {
+                    DeserializeIntoQueue(messageString);
+                }
+                catch (Exception exception)
+                {
+                    Debug.Log($"Failed to deserialize received message: {exception.Message}");
+                }
+            }
+
+            Debug.Log("WebSocket connection to server closed.");
+        }
+
+        private void DeserializeIntoQueue(string messageString)
+        {
+            var messageWithMessageType = JObject.Parse(messageString);
+
+            string messageType = null;
+            JToken serializedMessage = null;
+            foreach (var property in messageWithMessageType.Properties())
+            {
+                messageType = property.Name;
+                serializedMessage = property.Value;
+                break;
+            }
+
+            if (messageType == null || serializedMessage == null)
+            {
+                throw new ArgumentException(
+                    $"Expected JSON message to be wrapped in object with message type, but got: {messageString}"
+                );
+            }
+
+            switch (messageType)
+            {
+                case MessageType.Error:
+                    DeserializeAndEnqueue(serializedMessage, _errorMessages);
+                    break;
+                case MessageType.PlayerStatus:
+                    DeserializeAndEnqueue(serializedMessage, _playerStatusMessages);
+                    break;
+                case MessageType.LobbyJoined:
+                    DeserializeAndEnqueue(serializedMessage, _lobbyJoinedMessages);
+                    break;
+                case MessageType.SupportRequest:
+                    DeserializeAndEnqueue(serializedMessage, _supportRequestMessages);
+                    break;
+                case MessageType.OrderRequest:
+                    DeserializeAndEnqueue(serializedMessage, _orderRequestMessages);
+                    break;
+                case MessageType.OrdersReceived:
+                    DeserializeAndEnqueue(serializedMessage, _ordersReceivedMessages);
+                    break;
+                case MessageType.OrdersConfirmation:
+                    DeserializeAndEnqueue(serializedMessage, _ordersConfirmationMessages);
+                    break;
+                case MessageType.BattleResults:
+                    DeserializeAndEnqueue(serializedMessage, _battleResultsMessages);
+                    break;
+                case MessageType.Winner:
+                    DeserializeAndEnqueue(serializedMessage, _winnerMessages);
+                    break;
+                default:
+                    Debug.LogError($"Unrecognized message type received from server: {messageType}");
+                    break;
+            }
+        }
+
+        private static void DeserializeAndEnqueue<TMessage>(JToken serializedMessage, ConcurrentQueue<TMessage> queue)
+        {
+            var message = serializedMessage.ToObject<TMessage>();
+            if (message == null)
+            {
+                throw new ArgumentException($"Failed to deserialize message \"{serializedMessage}\"");
+            }
+
+            queue.Enqueue(message);
         }
     }
 }
